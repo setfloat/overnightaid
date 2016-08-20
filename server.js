3f5d61332ada7d13f2229c96dc5179e10225960a
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-08-20 11:52:25 -0700

    created server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.disable('x-powered-by');
+
+const morgan = require('morgan');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CSRF Protection
+app.use('/api', (req, res, next) => {
+  if (/json/.test(req.get('Accept'))) {
+    return next();
+  }
+
+  res.sendStatus(406);
+});
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// Routes to be inserted here
+
+app.use((_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port', port);
+});
