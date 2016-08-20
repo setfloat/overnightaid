6d973bdee89a874cc7ffe601941fae6530c21c10
Patrick Richardson <setfloat@gmail.com>
2016-08-20 13:54:35 -0700

    created order route file

---
+++ routes/orders.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { camelizeKeys, decamelizeKeys } = require('humps');
+const boom = require('boom');
+const checkAuth = require('../middleware');
+const ev = require('express-validation');
+const express = require('express');
+const knex = require('../knex');
+const validations = require('../validations/users');
+
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+router.post('orders', checkAuth, ev(validations.post), (req, res, next) => {
+  const {
+    addressFullName,
+    addressLine1,
+    addressLine2,
+    addressCity,
+    addressState,
+    addressZip
+  } = req.body;
+  const { userId } = req.token;
+  const newOrder = {
+    addressFullName,
+    addressLine1,
+    addressLine2,
+    addressCity,
+    addressState,
+    addressZip,
+    userId
+  };
+  const row = decamelizeKeys(newOrder);
+
+  knex('orders')
+    .insert(row, '*')
+    .then((rows) => {
+      const order = camelizeKeys(rows[0]);
+
+      res.send(order);
+    })
+    .catch((err) => next(err));
+});
+
+module.exports = router;
