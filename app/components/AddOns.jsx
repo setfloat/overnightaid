f79b4c1b148b5067f8280ff2742e9d2fd34d927b
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-08-24 14:09:40 -0700

    completed functionality of AddOns

--- app/components/AddOns.jsx
+++ app/components/AddOns.jsx
@@ -1,8 +1,5 @@
-import Divider from 'material-ui/Divider';
-import MenuItem from 'material-ui/MenuItem';
-import Paper from 'material-ui/Paper';
+import AddOn from 'components/AddOn';
 import React from 'react';
-import SelectField from 'material-ui/SelectField';
 import classNames from 'classnames';
 
 const styles = {
@@ -17,161 +14,81 @@ const styles = {
     fontWeight: 400
   },
   tabponent: {
-    marginTop: '50',
-    marginLeft: '50',
-    marginRight: '50',
-    paddingBottom: '40',
-    maxWidth: '700',
+    marginTop: '50px',
+    marginLeft: '50px',
+    marginRight: '50px',
+    paddingBottom: '40px',
+    maxWidth: 700,
     width: 'viewPort',
     alignContent: 'centerAlign'
-  },
-  personContent: {
-    fontSize: 24,
-    maxWidth: '95%',
-    padding: '5%',
-    margin: '20'
   }
 };
 
 const AddOns = React.createClass({
+  getInitialState() {
+    return {
+      addOns: []
+    };
+  },
+
+  componentWillReceiveProps(nextProps) {
+    const nextAddOns = nextProps.items.filter((items) => {
+      return items.category === 'optionables';
+    });
+
+    this.setState({ addOns: nextAddOns });
+  },
+
+  handleButtonTouchTap() {
+    const newCartItems = this.state.addOns.filter((item) => {
+      return item.quantity && item.quantity > 0;
+    });
+
+    this.props.addToCart(newCartItems);
+  },
+
+  updateQuantity(item, updatedQuantity) {
+    const nextAddOns = this.state.addOns.map((element) => {
+      if (item !== element) {
+        return element;
+      }
+
+      return Object.assign({}, item, { quantity: updatedQuantity });
+    });
+
+    this.setState({ addOns: nextAddOns });
+  },
+
   render() {
+    const { addOns } = this.state;
+
+    const styleFlexMain = {
+      marginBottom: '50px'
+    };
+
     const lrgBtnClassNames = classNames({
       btnLarge: true,
       wavesEffect: true
     });
 
-    const styleFlexMain = {
-      fontSize: 24,
-      marginBottom: '50px'
-    };
-
     return <div>
-      <div
-        style={styles.tabponent}
-        zDepth={3}
-      >
-        <h3 style={styles.headline}>
-          Add-ons: Anything else you would like to order?
-        </h3>
-        <Paper
-          rounded={false}
-          style={styles.personContent}
-          zDepth={2}
-        >
-          <h3 style={styles.headline}>Infant Pack
-            <SelectField
-              onChange={this.handleChange}
-              style={{ marginLeft: '20px' }}
-              value={1}
-            >
-              {/* <SelectField
-              onChange={this.handleChange}
-              value={this.state.value}> */}
-              <MenuItem
-                default
-                disabled
-                primaryText="Quantity"
-                value={1}
-              />
-              <MenuItem primaryText="1" value={2} />
-              <MenuItem primaryText="2" value={3} />
-              <MenuItem primaryText="3" value={4} />
-              <MenuItem primaryText="4" value={5} />
-            </SelectField>
-          </h3>
-          <Divider />
-        </Paper>
-        <Paper
-          rounded={false}
-          style={styles.personContent}
-          zDepth={2}
-        >
-          <h3 style={styles.headline}>Makeup Kit
-            <SelectField
-              onChange={this.handleChange}
-              style={{ marginLeft: '20px' }}
-              value={1}
-            >
-              {/* <SelectField
-              onChange={this.handleChange}
-              value={this.state.value}> */}
-              <MenuItem
-                default
-                disabled
-                primaryText="Quantity"
-                value={1}
-              />
-              <MenuItem primaryText="1" value={2} />
-              <MenuItem primaryText="2" value={3} />
-              <MenuItem primaryText="3" value={4} />
-              <MenuItem primaryText="4" value={5} />
-            </SelectField>
-          </h3>
-          <Divider />
-        </Paper>
-        <Paper
-          rounded={false}
-          style={styles.personContent}
-          zDepth={2}
-        >
-          <h3 style={styles.headline}>Dogs
-            <SelectField
-              onChange={this.handleChange}
-              style={{ marginLeft: '20px' }}
-              value={1}
-            >
-              {/* <SelectField
-              onChange={this.handleChange}
-              value={this.state.value}> */}
-              <MenuItem
-                default
-                disabled
-                primaryText="Quantity"
-                value={1}
-              />
-              <MenuItem primaryText="1" value={2} />
-              <MenuItem primaryText="2" value={3} />
-              <MenuItem primaryText="3" value={4} />
-              <MenuItem primaryText="4" value={5} />
-            </SelectField>
-          </h3>
-          <Divider />
-        </Paper>
-        <Paper
-          rounded={false}
-          style={styles.personContent}
-          zDepth={2}
-        >
-          <h3 style={styles.headline}>Cats
-            <SelectField
-              onChange={this.handleChange}
-              style={{ marginLeft: '20px' }}
-              value={1}
-            >
-              {/* <SelectField
-              onChange={this.handleChange}
-              value={this.state.value}> */}
-              <MenuItem
-                default
-                disabled
-                primaryText="Quantity"
-                value={1}
-              />
-              <MenuItem primaryText="1" value={2} />
-              <MenuItem primaryText="2" value={3} />
-              <MenuItem primaryText="3" value={4} />
-              <MenuItem primaryText="4" value={5} />
-            </SelectField>
-          </h3>
-          <Divider />
-        </Paper>
-        <input
-          className={lrgBtnClassNames}
-          style={styleFlexMain}
-          type="button"
-          value="Correct"
-        />
-      </div>
+      <h3 style={styles.headline}>
+        Optionables and AddOns
+      </h3>
+      {addOns.map((addOn, index) => {
+        return <AddOn
+          item={addOn}
+          key={index}
+          updateQuantity={this.updateQuantity}
+        />;
+      })}
+      <input
+        className={lrgBtnClassNames}
+        onTouchTap={this.handleButtonTouchTap}
+        style={styleFlexMain}
+        type="button"
+        value="Place An Order"
+      />
     </div>;
   }
 });
--- app/components/Catalog.jsx
+++ app/components/Catalog.jsx
@@ -4,6 +4,7 @@ const Catalog = React.createClass({
   render() {
     return <div>
       {React.cloneElement(this.props.children, {
+        addToCart: this.props.addToCart,
         items: this.props.items,
         cart: this.props.cart,
         familySize: this.props.familySize,
