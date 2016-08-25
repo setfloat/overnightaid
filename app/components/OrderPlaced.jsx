793f3220617cd1a0b7af4f93b7ad0bb77828317e
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-08-25 11:33:37 -0700

    styled OrderPlaced view

--- app/components/OrderPlaced.jsx
+++ app/components/OrderPlaced.jsx
@@ -2,12 +2,35 @@ import React from 'react';
 
 const OrderPlaced = React.createClass({
   render() {
-    return <div>
+    const styleFlexMain = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignContent: 'center',
+      flexWrap: 'nowrap',
+      height: 'viewPort'
+    };
+
+    return <div style={styleFlexMain}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <h2>Your Order Has Been Placed!</h2>
-      <h3>Your order number is: {this.props.orderId}</h3>
+      <h2>Your order number is: {this.props.orderId}</h2>
+      <h2>Thank you!</h2>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <h5>Please do not contact us if you have any issues with the order or
       various bugs on our site</h5>
-      <h2>Thank you!</h2>
     </div>;
   }
 });
